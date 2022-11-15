7c375caf380d9e62e1c8fb82d5f0a82221097b8f
Waleed Amjad <waleedamjad99@hotmail.com>
2022-11-15 11:22:36 +0500

    Update Groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,16 @@
 class GroupsController < ApplicationController
+    def index
+        return unless current_user
+    
+        @user = current_user
+        @groups = @user.groups.includes(:spendings)
+      end
+    def new
+        @groups =   Group.new
+    end
+
+    def show
+        @group = Group.find(params[:id])
+        @expenses = @group.expenses.order('created_at DESC')
+      end
 end
