b333546b22183d465899904a070a6fb1adc79209
Waleed Amjad <waleedamjad99@hotmail.com>
2022-11-15 15:36:20 +0500

    Fix linter errors

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,7 +1,7 @@
 class Expense < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   validates :icon, presence: true, length: { maximum: 250 }
-  
+
   belongs_to :user
   belongs_to :group
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   validates :icon, presence: true, length: { maximum: 250 }
-  
+
   belongs_to :user
   has_many :expenses, dependent: :delete_all
 end
