939bf5cc3b1224050cd2477a4c749a561b0dc129
Waleed Amjad <waleedamjad99@hotmail.com>
2022-11-15 11:27:54 +0500

    Update group controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,7 +3,7 @@ class GroupsController < ApplicationController
         return unless current_user
     
         @user = current_user
-        @groups = @user.groups.includes(:spendings)
+        @groups = @user.groups.includes(:expenses)
       end
     def new
         @groups =   Group.new
@@ -12,5 +12,20 @@ class GroupsController < ApplicationController
     def show
         @group = Group.find(params[:id])
         @expenses = @group.expenses.order('created_at DESC')
+      end
+
+      def create
+        @group = Group.new(group_params)
+        @group.user_id = current_user.id
+    
+        render :new unless @group.save
+    
+        redirect_to groups_path
+      end
+    
+      private
+    
+      def group_params
+        params.require(:group).permit(:name, :icon)
       end
 end
