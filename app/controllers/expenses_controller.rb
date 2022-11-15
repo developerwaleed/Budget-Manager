3df5ff59ea0338327c87f3cd7219fa3d756334e3
Waleed Amjad <waleedamjad99@hotmail.com>
2022-11-15 15:25:37 +0500

    Fix linter errors

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -7,7 +7,7 @@ class ExpensesController < ApplicationController
     @groups = params[:expense][:group_id].drop(1)
     @groups.each do |group|
       @expense = Expense.new(name: params[:expense][:name], amount: params[:expense][:amount],
-                               group_id: group.to_i, user_id: current_user.id)
+                             group_id: group.to_i, user_id: current_user.id)
       @expense.save
     end
     redirect_to group_path(@groups.first.to_i)
