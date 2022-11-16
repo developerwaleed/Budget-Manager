d8bc398807be8d9a2ab789e0ac36b84c797347c9
Waleed Amjad <waleedamjad99@hotmail.com>
2022-11-16 17:06:24 +0500

    Add authentication and test for user model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,6 +1,5 @@
 class Expense < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
-  validates :icon, presence: true, length: { maximum: 250 }
 
   belongs_to :user
   belongs_to :group
