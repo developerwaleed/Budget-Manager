0ae2e37331fddd9af9dd24e71a5a945f262d2aac
Waleed Amjad <waleedamjad99@hotmail.com>
2023-01-01 01:45:11 +0500

    Update deployment config v3

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,6 +1,8 @@
-
+#!/usr/bin/env bash
+# exit on error
 set -o errexit
 
 bundle install
-bundle exec rails db:migrate
-bundle exec rails db:seed
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
